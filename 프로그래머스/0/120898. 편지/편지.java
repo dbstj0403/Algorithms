deb3ad3ace09cdfae8fcad96806d36b0991ba531
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-02-24 23:42:13 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int howLong = message.length();
+        
+        answer = howLong * 2;
+        return answer;
+    }
+}
