173b8807b3ff0264f4c011f7e5ec04e84e7d5232
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-02-25 14:39:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(price >= 100000 && price < 300000){
+            price = price * 95 / 100;
+        }
+        else if(price >= 300000 && price < 500000){
+            price = price * 90 / 100;
+        }
+        else if(price >= 500000){
+            price = price * 80 / 100;
+        }
+        
+        answer = price;
+        
+        return answer;
+    }
+}
