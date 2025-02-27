d8d6ec5caae1d4316635cd0dd4847ff3fbbcd2c3
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-02-28 00:00:57 +0900

    [level 0] Title: 정수 부분, Time: 0.03 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        // 실수 flo의 정수 부분을 리턴
+        answer = (int) flo;
+        return answer;
+    }
+}
