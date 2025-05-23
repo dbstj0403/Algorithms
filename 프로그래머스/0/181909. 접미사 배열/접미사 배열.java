ed21d947509df269efb6f8304da9fb35f2000afd
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-05-23 10:23:30 +0900

    [level 0] Title: 접미사 배열, Time: 2.84 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        
+        int len = my_string.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i = len - 1; i >= 0; i--){
+            char c = my_string.charAt(i);
+            sb.insert(0, c);
+            System.out.println(sb);
+            list.add(sb.toString());
+        }
+        
+        Collections.sort(list);
+       
+        return list.toArray(new String[0]);
+    }
+}
