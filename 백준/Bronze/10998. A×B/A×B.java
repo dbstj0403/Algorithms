333aa3d4fd33148b2cbcd6642eb326bdc08f497c
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-05-27 17:40:12 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       // 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하여라
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.print(A * B);
+    }
+}
