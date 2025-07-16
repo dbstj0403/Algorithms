bc6fb017f5936bb022eacb2e3d4500e74d755e6e
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-07-16 11:20:30 +0900

    [level 1] Title: K번째수, Time: 1.04 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,18 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
-        for (int i = 0; i < commands.length; i++) {
-            // 배열 슬라이싱 (1-based index → 0-based index 변환)
-            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-
-            // 정렬
-            Arrays.sort(arr);
-
-            // k번째 원소 선택 (1-based index → 0-based index 변환)
-            answer[i] = arr[commands[i][2] - 1];
+        
+        for(int i = 0; i < commands.length; i++){
+            int[] command = commands[i];
+            int[] sliced = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(sliced);
+            int target = sliced[command[2] - 1];
+            answer[i] = target;
         }
-
+        
         return answer;
     }
 }
