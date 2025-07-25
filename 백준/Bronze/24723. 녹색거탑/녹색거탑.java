4c1a6666f4ff11d30aaf7f864a50ed7f2119a815
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-07-25 09:27:02 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        double result = Math.pow(2, N);
+        System.out.println((int) result);
+    }
+}
