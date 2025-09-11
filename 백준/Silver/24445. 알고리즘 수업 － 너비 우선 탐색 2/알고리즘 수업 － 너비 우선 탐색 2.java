98ee1b7ce2994bdbe3504949098b6acb1bb5f438
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-09-11 15:09:59 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1580 ms, Memory: 100780 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+    static Queue<Integer> q = new LinkedList<>();
+    static int[] orders;
+    static int count = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        orders = new int[N + 1];
+
+        for(int i = 0; i <= N; i++) {
+            edges.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            addEdge(a, b);
+        }
+
+        for(ArrayList<Integer> edge : edges) {
+            Collections.sort(edge, Collections.reverseOrder());
+        }
+
+        bfs(R);
+
+        for(int i = 1; i <= N; i++) {
+            System.out.println(orders[i]);
+        }
+    }
+
+    static void addEdge(int a, int b) {
+        edges.get(a).add(b);
+        edges.get(b).add(a);
+    }
+
+    static void bfs(int start) {
+        visited[start] = true;
+        orders[start] = count++;
+        q.add(start);
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for(int edge : edges.get(cur)) {
+                if(!visited[edge]){
+                    visited[edge] = true;
+                    orders[edge] = count++;
+                    q.add(edge);
+                }
+            }
+        }
+    }
+}
