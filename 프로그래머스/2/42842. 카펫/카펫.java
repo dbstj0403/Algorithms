dcf9ad923d4296902bb87a7e88e7be4e3aacf3b0
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-09-18 22:14:48 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        // 가로가 세로보다 넓거나 같아야 한다.
+        
         int sum = brown + yellow;
-
-        for (int height = 3; height <= Math.sqrt(sum); height++) {
-            if (sum % height == 0) { // 약수인지 확인
+        for(int height = 3; height <= Math.sqrt(sum); height++) {
+            if(sum % height == 0) {
                 int width = sum / height;
-
-                // 안쪽 노란색 칸 개수 확인
-                if ((width - 2) * (height - 2) == yellow) {
+                
+                if((width - 2) * (height - 2) == yellow) {
                     return new int[] {width, height};
                 }
             }
