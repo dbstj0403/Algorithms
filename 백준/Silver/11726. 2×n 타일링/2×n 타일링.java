a700bcd8136adc036d9e9d9862a61e4c5ed0c7af
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-09-30 18:38:52 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -14,7 +14,7 @@ public class Main {
 
         dp[0] = 0;
         dp[1] = 1;
-        if(n >=2 ) dp[2] = 2;
+        if(n >= 2) dp[2] = 2;
 
         for(int i = 3; i <= n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
