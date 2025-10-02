3b6b09e92341d9b098be91abdb71c2d860bad5c2
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-10-02 15:34:24 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int answer = 0;
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        answer = (a * a + b * b + c * c + d * d + e * e) % 10;
+
+        System.out.println(answer);
+    }
+}
