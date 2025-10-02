98fdad22900583f521dddb2e08d0fcf8db6d4d87
윤서 <113417326+dbstj0403@users.noreply.github.com>
2025-10-02 15:15:25 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        // 최대 N = 40
+        int[][] dp = new int[41][2];
+        dp[0][0] = 1; // fibonacci(0) 호출될 때 0이 1번
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        }
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
